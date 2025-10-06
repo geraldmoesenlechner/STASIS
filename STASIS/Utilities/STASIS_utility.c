4b6be293da42aff673c9852e25ae8415200e59be
Gerald Mösenlechner <52880460+geraldmoesenlechner@users.noreply.github.com>
2025-10-06 13:04:37 +0200

    Remove copyright notice from STASIS_utility.c
    Removed copyright information from the utility file.

--- STASIS/Utilities/STASIS_utility.c
+++ STASIS/Utilities/STASIS_utility.c
@@ -3,15 +3,6 @@
 * @author  Gerald Mösenlechner (gerald.moesenlechner@univie.ac.at)
 * @date    October, 2019
 *
-* @copyright
-* This program is free software; you can redistribute it and/or modify it
-* under the terms and conditions of the GNU General Public License,
-* version 2, as published by the Free Software Foundation.
-*
-* This program is distributed in the hope it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-* more details.
 * @brief Utility functions for the data simulator for the ARIEL space mission.
 *
 *
