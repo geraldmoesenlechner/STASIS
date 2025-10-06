cd506a558ceee4c1b44e0638497fde95f08c50df
Gerald Mösenlechner <52880460+geraldmoesenlechner@users.noreply.github.com>
2025-10-06 13:03:44 +0200

    Remove copyright notice from STASIS_detector.c
    Removed copyright information from the file header.

--- STASIS/Detector_features/STASIS_detector.c
+++ STASIS/Detector_features/STASIS_detector.c
@@ -3,15 +3,6 @@
 * @author  Gerald Mösenlechner (gerald.moesenlechner@univie.ac.at)
 * @date    October, 2021
 *
-* @copyright
-* This program is free software; you can redistribute it and/or modify it
-* under the terms and conditions of the GNU General Public License,
-* version 2, as published by the Free Software Foundation.
-*
-* This program is distributed in the hope it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-* more details.
 * @brief Functions for the generation of detector features, such as bias, dark and flats.
 *
 *
